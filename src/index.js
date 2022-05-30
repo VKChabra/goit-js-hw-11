eb71af206b1390248e029de957e4cecf94d49d36
Vlad Koblay <vkchabraww@gmail.com>
2022-05-30 15:08:58 +0300

    patch 0.4
    data is now loading

---
+++ src/index.js
@@ -0,0 +1,71 @@
+import './sass/main.scss';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import './js/fetchImages'
+import {Notify} from 'notiflix';
+import fetchImages from "./js/fetchImages";
+
+const refs = {
+    form: document.querySelector('.top-form'),
+    headerFormInput: document.querySelector('.top-form__input'),
+    headerFormSearchBtn: document.querySelector('.top-form__search-btn'),
+    gallery: document.querySelector('.gallery')
+}
+const { headerFormInput, headerFormSearchBtn, gallery, form } = refs;
+
+const trimmedInputValue = () => headerFormInput.value.trim();
+
+const searchImages = async (e) => {
+    e.preventDefault();
+    if (trimmedInputValue() === '') {
+        return Notify.info('Type anything first please, will ya?');
+    } else {
+        try {
+            const fetchImg = await fetchImages(trimmedInputValue());
+            return makeGalleryItemsMarkup(fetchImg);
+        } catch (error) {
+            () => {
+                return console.log(error);
+            }
+        }
+    }
+}
+
+form.addEventListener('submit', searchImages)
+
+const makeGalleryItemsMarkup = data => {
+    const arrayOfResults = data.data.hits;
+    checkIfFound(data);
+    const markupOfResults = arrayOfResults.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${largeImageURL}">
+            <img
+            class="gallery__image"
+            src="${webformatURL}"
+            alt="${tags}"
+            />
+        </a>
+        <ul class="gallery__item-data">
+            <li class='gallery__received-data>Likes
+            <br>${likes}</li>
+            <li class='gallery__received-data>Views
+            <br>${views}</li>
+            <li class='gallery__received-data>Comments
+            <br>${comments}</li>
+            <li class='gallery__received-data>Downloads
+            <br>${downloads}</li>
+        </ul>
+        </div>`;
+    }).join('');
+    return insertImages(markupOfResults);
+};
+
+function checkIfFound(e) {
+    if (e.data.hits.length === 0) {
+        return Notify.failure('Nothing was found')
+    }
+}
+
+function insertImages(images) {
+    gallery.insertAdjacentHTML('beforeend', images);
+}
